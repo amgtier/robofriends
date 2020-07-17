bb8082691df991f39067e11e79bd03bde1166ec7
Tzu-Hsiang Chao <amgtier@vatroc.net>
2020-07-17 23:33:41 +0800

    Updates

---
+++ precache-manifest.37e41e9d67588f73534588680040adf9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb2a8db40eba08dd6b1d039cf4823142",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "7e4f46db1e3e099b904a",
+    "url": "/robofriends/static/css/main.31b4bcd0.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robofriends/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e4f46db1e3e099b904a",
+    "url": "/robofriends/static/js/main.9c1a11cd.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
